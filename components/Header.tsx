a78beb5ddebfb750259c2b9d244edc64400fa9e8
pinnc20-debug <pinnc20@gmail.com>
2026-02-06 19:50:44 +0700

    refactor: Disable unused variable checks
    Disabling `noUnusedLocals` and `noUnusedParameters` in `tsconfig.json` to allow for more flexible component development and reduce build noise, especially during initial development phases.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Header: React.FC = () => {
+export const Header = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white/90 backdrop-blur-xl border-b border-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
